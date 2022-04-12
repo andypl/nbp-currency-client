0e4f5c7e894aed498858f282eedaaa07474e7819
anc <andrzej.czerniak@gmail.com>
2022-04-12 21:00:53 +0200

    Initial commit

---
+++ src/main/java/pl/info/czerniak/nbpcurrencyclient/NbpCurrencyClientApplication.java
@@ -0,0 +1,13 @@
+package pl.info.czerniak.nbpcurrencyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpCurrencyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpCurrencyClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/info/czerniak/nbpcurrencyclient/NbpCurrencyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.info.czerniak.nbpcurrencyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpCurrencyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
